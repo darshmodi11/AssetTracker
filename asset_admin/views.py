67aa1a4fa8764cfdd7d19312f6df31a7670e944f
darsh_modi11 <darsh.modi@neosoftmail.com>
2024-10-17 17:46:58 +0530

    feat: Implemented CRUD for asset type and added Bar chart into the Index page.

--- asset_admin/views.py
+++ asset_admin/views.py
@@ -3,14 +3,15 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.db.models import Count, Q
-from django.db.models.query_utils import select_related_descend
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import View, ListView, CreateView, TemplateView
+from django.views.generic import View, CreateView, TemplateView, \
+    UpdateView
 
-from asset_admin.forms import LoginForm, CreateAssetTypeForm, CreateAssetForm
-from asset_admin.models import AssetType, Asset
+from asset_admin.forms import LoginForm, CreateAssetTypeForm, CreateAssetForm, \
+    AssetImageFormset
+from asset_admin.models import AssetType, Asset, AssetImage
 
 
 # Create your views here.
@@ -54,11 +55,16 @@ class IndexView(LoginRequiredMixin, View):
             asset_count=Count('assets')).values_list('asset_count', flat=True)
         assets_type_name = AssetType.objects.annotate(
             asset_count=Count('assets')).values_list('name', flat=True)
+        total_assets = Asset.objects.count()
+        active_assets = Asset.objects.filter(is_active=True).count()
+        inactive_assets = total_assets - active_assets
 
         return render(self.request,
                       'asset_admin/index.html',
                       context={'asset_types': list(assets_type_name),
-                               'assets_count': list(assets_count_by_type)
+                               'assets_count': list(assets_count_by_type),
+                               'active_assets': active_assets,
+                               'inactive_assets': inactive_assets
                                }
                       )
 
@@ -114,7 +120,6 @@ class CreateAssetTypeView(LoginRequiredMixin, CreateView):
         return super().form_invalid(form)
 
 
-
 class AssetListView(LoginRequiredMixin, TemplateView):
     login_url = reverse_lazy("asset_admin:admin_login")
     template_name = 'asset_admin/asset_list.html'
@@ -165,8 +170,81 @@ class CreateAssetView(LoginRequiredMixin, CreateView):
     template_name = 'asset_admin/create_new_asset.html'
     success_url = reverse_lazy("asset_admin:asset_list")
 
-    def form_valid(self, form):
-        return super().form_valid(form)
+    def get(self, request, *args, **kwargs):
+        formset = AssetImageFormset(prefix="asset_image",
+                                    queryset=AssetImage.objects.none())
+        return render(request,
+                      self.template_name,
+                      context={
+                          "image_formset": formset,
+                          "form": self.form_class,
+                      })
+
+    def post(self, request, *args, **kwargs):
+        formset = AssetImageFormset(self.request.POST,
+                                    files=self.request.FILES,
+                                    prefix="asset_image"
+                                    )
+        form = CreateAssetForm(request.POST)
+
+        import pdb;
+        pdb.set_trace()
 
     def form_invalid(self, form):
         return super().form_invalid(form)
+
+
+def get_asset_type_detail(request, pk):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        asset_type = AssetType.objects.filter(pk=pk).values().first()
+        return JsonResponse(status=200,
+                            data={
+                                "success": True,
+                                "data": asset_type
+                            }
+                            )
+    else:
+        return JsonResponse(status=400,
+                            data={
+                                "success": False,
+                                "message": "Invalid request received."
+                            })
+
+
+def delete_asset_type(request, pk):
+    if (request.headers.get('x-requested-with') == 'XMLHttpRequest'
+            and request.method == 'POST'):
+        try:
+            AssetType.objects.get(pk=pk).delete()
+
+            return JsonResponse(status=200,
+                                data={
+                                    "success": True,
+                                    "message": "Asset type deleted successfully."
+                                })
+
+        except AssetType.DoesNotExist:
+            return JsonResponse(status=404,
+                                data={
+                                    "success": False,
+                                    "message": "Asset type does not exist."
+                                })
+
+
+class AssetTypeUpdateView(UpdateView):
+    form_class = CreateAssetTypeForm
+    model = AssetType
+    success_url = reverse_lazy('asset_admin:asset_type_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({'success': True,
+                                 'message': 'Asset type updated successfully!'})
+        return response
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({'success': False, 'errors': form.errors})
+        return response
